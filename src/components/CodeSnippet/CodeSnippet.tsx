cc517f276c1f395711eac36993dd0092528498a4
Dergachev <dergachev@hotmail.com>
2019-01-05 03:17:13 +0700

    feat(CodeSnippet): line numbers

--- src/components/CodeSnippet/CodeSnippet.tsx
+++ src/components/CodeSnippet/CodeSnippet.tsx
@@ -2,16 +2,31 @@ import * as React from 'react'
 import * as styles from './CodeSnippet.css'
 
 interface ICodeSnippetProps {
+    editable?: boolean,
     code: string
 }
 
 function CodeSnippet(props: ICodeSnippetProps) {
     const lines = props.code.split('\n')
+    const lineNumberWidth = getLineNumberWidth(lines.length)
     return (
-        <div contentEditable className={styles.Container}>
-            {lines.map(line => <div>{line}</div>)}
+        <div className={styles.Container}>
+            {lines.map((line, index) =>
+                <div className={styles.Line}>
+                    <div className={styles.LineNumber} style={lineNumberWidth}>
+                        {index}
+                    </div>
+                    <pre className={styles.LineCode}>
+                        {line || '\n'}
+                    </pre>
+                </div>
+            )}
         </div>
     )
+}
+
+function getLineNumberWidth(lines: number) {
+    return { minWidth: 8 + lines.toString().length * 8 }
 }
 
 export default CodeSnippet
