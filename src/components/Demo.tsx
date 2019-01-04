9c653e220705b75cc994845853ecbf68caac2108
Dergachev <dergachev@hotmail.com>
2019-01-05 04:45:31 +0700

    feat(Demo): self snippet, css snippet sample

--- src/components/Demo.tsx
+++ src/components/Demo.tsx
@@ -2,14 +2,28 @@ import * as React from 'react'
 import CodeSnippet from 'components/CodeSnippet/CodeSnippet'
 import * as styles from './Demo.css'
 
-const src = require('!raw-loader!components/CodeSnippet/CodeSnippet')
+const DemoSrc = require('!raw-loader!components/Demo')
+const CodeSnippetSrc = require('!raw-loader!components/CodeSnippet/CodeSnippet')
+const CodeSnippetCss = require('!raw-loader!components/CodeSnippet/CodeSnippet.css')
 
 function Demo() {
     return (
         <div className={styles.Container}>
-            <h1 className={styles.PageTitle}>Components</h1>
-            <h2 className={styles.ComponentTitle}>Code Snippet</h2>
-            <CodeSnippet code={src} />
+            <h1 className={styles.PageTitle}>
+                Components
+            </h1>
+            <h2 className={styles.ComponentTitle}>
+                Demo
+            </h2>
+            <CodeSnippet code={DemoSrc} />
+            <h2 className={styles.ComponentTitle}>
+                Code Snippet - TSX
+            </h2>
+            <CodeSnippet code={CodeSnippetSrc} />
+            <h2 className={styles.ComponentTitle}>
+                Code Snippet - CSS
+            </h2>
+            <CodeSnippet code={CodeSnippetCss} />
         </div>
     )
 }
