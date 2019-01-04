1cf751cfc24ad84d00c295c1329977c312165934
Dergachev <dergachev@hotmail.com>
2019-01-04 19:40:34 +0700

    chore: 'inline-source-map' -> 'source-map' to reduce bundle size

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = {
   entry: './src/index.tsx',
-  devtool: 'inline-source-map',
+  devtool: 'source-map',
   module: {
     rules: [
       {
