396927851beed1c65ccb914eccb29938adaadfd6
Dergachev <dergachev@hotmail.com>
2019-01-06 23:24:17 +0700

    style: webpack config now under tslint control

---
+++ tslint.js
@@ -0,0 +1,26 @@
+/* jsRules: true still applying 'object-literal-sort-keys' rule for some reason */
+const rules = {
+    'ter-indent': [true, 4],
+    'semicolon': {
+        'options': 'never'
+    },
+    'quotemark': {
+        'options': ['single', 'jsx-double']
+    },
+    'arrow-parens': false,
+    'trailing-comma': false,
+    'ordered-imports': false,
+    'no-var-requires': false,
+    'no-console': false,
+    'object-literal-sort-keys': false,
+}
+
+module.exports = {
+    extends: [
+        'tslint:recommended',
+        'tslint-eslint-rules'
+    ],
+    rules,
+    jsRules: rules,
+    rulesDirectory: []
+}
--- webpack.config.js
+++ webpack.config.js
@@ -3,54 +3,54 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const TerserPlugin = require('terser-webpack-plugin')
 
 const cssRules = [
-  { loader: 'style-loader' },
-  {
-    loader: 'css-loader',
-    options: {
-      modules: true,
-      localIdentName: '[name]__[local]___[hash:base64:5]'
+    { loader: 'style-loader' },
+    {
+        loader: 'css-loader',
+        options: {
+            modules: true,
+            localIdentName: '[name]__[local]___[hash:base64:5]'
+        }
     }
-  }
-] 
+]
 
 module.exports = {
-  entry: './src/index.tsx',
-  devtool: 'source-map',
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.css?$/,
-        use: cssRules
-      }
-    ]
-  },
-  resolve: {
-    modules: ['src', 'node_modules'],
-    extensions: ['.tsx', '.ts', '.js']
-  },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  plugins: [
-    new HtmlWebpackPlugin({ hash: true, template: './src/templates/index.html' })
-  ],
-  optimization: {
-    minimizer: [
-      new TerserPlugin({
-        parallel: true,
-        sourceMap: true,
-        terserOptions: {
-          ecma: 8,
-          mangle: false,
-          keep_classnames: true
-        },
-      }),
-    ]
-  }
+    entry: './src/index.tsx',
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css?$/,
+                use: cssRules
+            }
+        ]
+    },
+    resolve: {
+        modules: ['src', 'node_modules'],
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new HtmlWebpackPlugin({ hash: true, template: './src/templates/index.html' })
+    ],
+    optimization: {
+        minimizer: [
+            new TerserPlugin({
+                parallel: true,
+                sourceMap: true,
+                terserOptions: {
+                    ecma: 8,
+                    mangle: false,
+                    keep_classnames: true
+                },
+            }),
+        ]
+    }
 }
